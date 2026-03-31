2a7827db9096317eabcaf0c8d83ca3c420daaf2e
cultbrandalicia-png <cultbrand.alicia@gmail.com>
2026-03-31 20:44:03 +0200

    Fix: corregir comillas JSX en app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang=\"es\">
+    <html lang="es">
       <body className={inter.className}>
-        <div className=\"flex\">
+        <div className="flex">
           <Sidebar />
-          <main className=\"flex-1 ml-64 p-8 bg-gray-50 min-h-screen\">
+          <main className="flex-1 ml-64 p-8 bg-gray-50 min-h-screen">
             {children}
           </main>
         </div>
